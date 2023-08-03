572f98c5aaae3562f9a7c2888806a75da25c5d5f
Jimmy <jiaming0708@gmail.com>
2023-08-03 21:56:24 +0800

    fetch list data

---
+++ app/interface/assets.tsx
@@ -0,0 +1,14 @@
+export interface Assets {
+  assets: Asset[];
+}
+
+export interface Asset {
+  id: number;
+  token_id: string;
+  image_url: string;
+  name: string;
+  description: string;
+  asset_contract: {
+    address: string;
+  };
+}
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,41 +1,29 @@
-import type { V2_MetaFunction } from "@remix-run/node";
+import { json } from '@remix-run/node';
+import { useLoaderData, Link as RemixLink } from '@remix-run/react';
+import { Image, Heading, Flex, Link as ChakraLink } from '@chakra-ui/react';
+import type { Assets } from '../interface/assets';
 
-export const meta: V2_MetaFunction = () => {
-  return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
-  ];
-};
+export async function loader() {
+  const res = await fetch(
+    `https://testnets-api.opensea.io/api/v1/assets?owner=0x85fD692D2a075908079261F5E351e7fE0267dB02`,
+  );
+  return json<Assets>(await res.json());
+}
 
 export default function Index() {
+  const { assets } = useLoaderData<typeof loader>();
+
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
+    <Flex flexWrap='wrap'>
+      {assets.map((asset) => (
+        <ChakraLink key={asset.id} width='50%' as={RemixLink} to={`/${asset.asset_contract.address}/${asset.token_id}`}>
+          <Image
+            src={asset.image_url}
+            alt={asset.description}
+          />
+          <Heading>{asset.name}</Heading>
+        </ChakraLink>
+      ))}
+    </Flex>
   );
 }
